2b6a89eac85e17fcbb8d5aae0c65b784ca21d2f6
Neil Duffy <neiladuffy@gmail.com>
2012-02-15 12:58:09 -0600

    added centering

--- js/splayer.js
+++ js/splayer.js
@@ -6,7 +6,18 @@ var splayer = function(selector) {
 		mask: document.createElement("li"),
 		expanded: false,
 		height: 0,
-		width: 0
+		open_height: 0,
+		width: 0,
+		top_offset: {
+			last_break: 0,
+			current_row: 0,
+			total: 0,
+			rows: {
+				0: {
+					width: 0
+				}
+			}
+		}
 	};
 	
 	this.init();
@@ -17,10 +28,15 @@ splayer.prototype = {
 			this.list.images[image].start_top = ((this.list.height - this.list.images[image].height) / 2);
 			this.list.images[image].start_position = Math.floor((this.list.width - this.list.images[image].width) / 2);
 			this.list.images[image].hover_position += this.list.images[image].start_position;
+			this.list.images[image].open_top += this.list.images[image].start_top;
 			
 			this.list.images[image].node.style.top = this.list.images[image].start_top + "px";
 			this.list.images[image].node.style.left = this.list.images[image].start_position + "px";
 		}
+		for (row in this.list.top_offset.rows) {
+			this.list.open_height += this.list.top_offset.rows[row].height + 10;
+		}
+		this.list.open_height += 80;
 	},
 	roll_in: function () {
 		if (!this.list.expanded) {
@@ -44,15 +60,16 @@ splayer.prototype = {
 			this.list.node.style.zIndex = "9999";
 			this.list.node.className = "splayer expanded";
 			
-			this.list.mask.style.height = (this.list.height + 80) + "px";
-			this.list.mask.style.width = (this.win.width - 35) + "px";
+			this.list.mask.style.height = this.list.open_height + "px";
+			this.list.mask.style.width = this.win.width + "px";
 			this.list.mask.style.top = (10 - this.list.node.offsetTop) + "px";
 			this.list.mask.style.left = (10 - this.list.node.offsetLeft) + "px";
 			this.list.mask.style.background = "rgba(0,0,0,.5)";
 			
 			for (image in this.list.images) {
-				this.list.images[image].node.style.top = (50 - this.list.node.offsetTop + this.list.images[image].start_top) + "px";
-				this.list.images[image].node.style.left = this.list.images[image].open_position - this.list.node.offsetLeft + "px";
+				var xtra_space = (this.win.width - this.list.top_offset.rows[this.list.images[image].row].width) / 2;
+				this.list.images[image].node.style.top = (50 - this.list.node.offsetTop + this.list.images[image].open_top) + "px";
+				this.list.images[image].node.style.left = this.list.images[image].open_position - this.list.node.offsetLeft + xtra_space + "px";
 			}
 		}
 		else {
@@ -77,9 +94,9 @@ splayer.prototype = {
 		var that = this,
 			images = [],
 			child_lis = this.list.node.getElementsByTagName("li"),
-			full_offset = 50;
+			full_offset = 0;
 		
-		this.win.width = window.innerWidth;
+		this.win.width = window.innerWidth - 35;
 		
 		this.list.node.className = "splayer";
 		
@@ -104,6 +121,14 @@ splayer.prototype = {
 		});
 		
 		for (var i = 0; i < images.length; i++) {
+			if ((full_offset + images[i].offsetWidth) > (this.win.width - 120)) {
+				this.list.top_offset.total += this.list.top_offset.rows[this.list.top_offset.current_row].height + 10;
+				this.list.top_offset.current_row += 1;
+				this.list.top_offset.rows[this.list.top_offset.current_row] = {};
+				this.list.top_offset.rows[this.list.top_offset.current_row].width = 0;
+				full_offset = 50;
+			}
+		
 			this.list.images[i] = {
 				node: images[i],
 				parent: images[i].parentElement,
@@ -112,15 +137,18 @@ splayer.prototype = {
 				area: images[i].offsetHeight * images[i].offsetWidth,
 				z_index: 9999 - Math.floor((images[i].offsetHeight * images[i].offsetWidth) / 1000),
 				start_top: 0,
+				open_top: this.list.top_offset.total,
 				start_position: 0,
 				hover_position: i * 5,
-				open_position: full_offset
+				open_position: full_offset,
+				row: this.list.top_offset.current_row
 			};
 			
 			//this.list.mask.appendChild(this.list.images[i].node.parentElement);
 			
 			this.list.images[i].node.style.zIndex = this.list.images[i].z_index;
-			
+			this.list.top_offset.rows[this.list.top_offset.current_row].height = (this.list.height < this.list.images[i].height) ? this.list.images[i].height : this.list.height;
+			this.list.top_offset.rows[this.list.top_offset.current_row].width += parseInt(this.list.images[i].width,10) + 10;
 			this.list.height = (this.list.height < this.list.images[i].height) ? this.list.images[i].height : this.list.height;
 			this.list.width = (this.list.width < this.list.images[i].width) ? this.list.images[i].width : this.list.width;
 			
